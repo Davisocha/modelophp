0f685c73572ae56c44aaf4f2132e63674cdff839
DAVI ARAUJO SOCHA <davi.asocha@senacsp.edu.br>
2025-02-11 17:22:47 -0300

    feito os destaques

--- produtos_destaque.php
+++ produtos_destaque.php
@@ -1,67 +1,51 @@
-<?php 
-// arquivo de conexão de banco
-
-
-// consulta para trazer os dados se filtar
-
-
+<?php
+include 'conn/connect.php';
+$lista = $conn->query("select * from vw_produtos where destaque = 'sim'");
+$row_produto = $lista->fetch_assoc();
+$num_linhas = $lista->num_rows; //ele pergunta quantas linhas retornadas nesse num_rows
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/estilo.css">
-    <title>Detalhes Produto</title>
-</head>
-<body class="fundofixo">
-    
-    <div class="container">
-        <h2 class="breadcrumb alert-danger">
-            <a href="index.php">
-                <button class="btn btn-danger">
-                    <span class="glyphicon glyphicon-chevron-left"></span>
-                </button>
-                <!-- use a tag Nicolas <sddv> -->
-                <strong>Detalhes do Produto</strong>
-            </a>
-        </h2>
-        <div class="row">
-           
-                <div class="col-sm-12 col-md-12">
-                    <div class="thumbnail ">
-                        <a href="">
-                            <img 
-                                src="images/" 
-                                alt="" 
-                                class="img-responsive img-rounded" 
-                                style="height: 20em ;">
+<!-- Mostrar se a consulta retornar vazio -->
+<?php
+if ($num_linhas == 0) { ?><!--agora de baixo dentro desse if vai mostrar na tela!-->
+    <h2 class="breadcrumb alert-danger">Não há produtos em destaque!</h2>
+<?php } ?>
+<!-- mostrar se a consulta retornou produtos -->
+<?php if ($num_linhas > 0)/*ele pergubta se o numeros de linha é maior que zero, se caso não for ele não ira mostrar os destaques*/ { ?>
+    <h2 class="breadcrumb alert-danger">
+        Destaques
+    </h2>
+    <div class="row"><!--  LINHA  -->
+        <?php do { //faça enquanto ?>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail ">
+                <a href="produto_detalhes.php?id=<?php echo $row_produto['id'];//ele vai puxar o id dos produtos em destaque?>"><!--quando clicar ele vai trazer até o produto-->
+                    <img src="images/<?php echo $row_produto['imagem']?>" alt="" class="img-responsive img-rounded">
+                </a>
+                <div class="caption text-right bg-danger">
+                    <h3 class="text-danger">
+                        <strong><?php echo $row_produto['descricao']?></strong>
+                    </h3>
+                    <p class="text-warning">
+                        <strong><?php echo mb_strimwidth($row_produto['resumo'],0,40,'...')//A função mb_strimwidth() no PHP é usada para truncar uma string até uma largura específica (em caracteres) e, opcionalmente, adicionar um sufixo (como uma elipse) caso a string ultrapasse essa largura.?></strong>
+                    </p>
+                    <p class="text-left">
+
+                    </p>
+                    <p>
+                        <button class="btn btn-default disabled" role="button" style="cursor: default;">
+                            <?php echo "R$ ".number_format($row_produto['valor'],2,',','.');//ele mostra 2 que são os centavos nesse caso e no lugar da virgula ele vai substituir de , para .?>
+                        </button>
+                        <?php echo $row_produto['id']?>
+                        <a href="produto_detalhes.php?id=">
+                            <span class="hidden-xs">Saiba mais..</span>
+                            <span class="hidden-xs glyphicon glyphicon-eye-open" aria-hidden="true"></span>
                         </a>
-                        <div class="caption text-center">
-                            <h3 class="text-danger">
-                                <strong></strong>
-                            </h3>
-                            <p class="text-warning">
-                                <strong></strong>
-                            </p>
-                            <p class="text-center">
-                                <strong></strong>
-                            </p>
-                            <p>
-                                <a href="index.php" class="btn btn-danger" role="button">
-                                    <span class="hidden-xs">Retornar</span>
-                                    <span class="visible-xs glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                                </a>
-                            </p>
-                        </div>
-                    </div>
-                </div>    
-             
+                    </p>
+                </div>
+            </div>
+
         </div>
+<?php }while($row_produto=$lista->fetch_assoc());//se rows produtos lista retornar uma proxima linha se retornar falso ele para?>
     </div>
-    
-</body>
-</html>
+<?php } ?>
